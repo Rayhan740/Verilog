875136f5d7cb51a454a00e7fd65aa541617a27cb
Rayhan <79773027+Rayhan740@users.noreply.github.com>
2022-10-04 03:55:14 +0600

    Create Half adder.v

---
+++ Half adder.v
@@ -0,0 +1,16 @@
+module main;
+  reg A,B;
+  wire S,C;
+  
+  xor xor1(S,A,B);
+  and and1(C,A,B);
+  
+  initial 
+    begin
+      A = 0;
+      B = 1;
+      #5; // Wait 5 times unit
+      $display("SUM = ",S);
+      $display("Carry = ",C);
+    end
+endmodule
